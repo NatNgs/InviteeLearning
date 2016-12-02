44cc5ce0a54ee57bd54a0981df798472faf30ffb
nn200057 <nathael.nogues@hotmail.fr>
2016-12-02 16:26:53 +0100

    Add testing method

--- NaiveBayes/src/nb/ProbaSet.java
+++ NaiveBayes/src/nb/ProbaSet.java
@@ -10,8 +10,7 @@ public class ProbaSet {
     // [ms][type][interval]
     private List<List<List<Probability>>> probabilities = new ArrayList<>();
 
-    public void treatFile(String fileName, boolean isACut, double data[/*500*/][/*6*/]) {
-
+    public void learn(String fileName, boolean isACut, double data[/*500*/][/*6*/]) {
         for(int ms=0; ms<data.length; ms++) {
             while(probabilities.size() <= ms) {
                 probabilities.add(new ArrayList<>());
@@ -35,6 +34,54 @@ public class ProbaSet {
                 p.addFileAs(fileName,isACut);
             }
         }
+    }
 
+    public double test(double data[/*500*/][/*6*/]) {
+        double probaMTrue = 1;
+        double probaMFalse = 1;
+        double probaATrue = 0;
+        double probaAFalse = 0;
+
+        for(int ms=0; ms<data.length; ms++) {
+            if(probabilities.size() <= ms) {
+                continue; // unknown probabilities
+            }
+            List<List<Probability>> l_ms = probabilities.get(ms);
+
+            double ms_data[/*6*/] = data[ms];
+            for(int type=0; type<ms_data.length; type++) {
+                if(l_ms.size() <= type) {
+                    continue; // unknown probabilities
+                }
+                List<Probability> l_type = l_ms.get(ms);
+
+                double value = ms_data[type];
+                int interval = Probability.intervalIndexFor(type, value);
+                if(l_type.size() <= interval) {
+                    continue; // unknown probabilities
+                }
+
+                Probability p = l_type.get(interval);
+
+                value = p.getProbability(true);
+                probaATrue += value;
+                probaMTrue *= value;
+
+                value = p.getProbability(false);
+                probaAFalse += value;
+                probaMFalse *= value;
+            }
+        }
+
+        if(probaMTrue==probaMFalse) {
+            if(probaATrue == probaAFalse) {
+                return .5; // we don't now
+            }
+            else {
+                return probaATrue/(probaATrue+probaAFalse);
+            }
+        } else {
+            return probaMTrue/(probaMTrue+probaMFalse);
+        }
     }
 }
